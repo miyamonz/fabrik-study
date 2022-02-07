63f7b317b8fd4dd3ce982cba33618946e979d7e9
miyamonz <m5813k@gmail.com>
2022-02-08 02:58:54 +0900

    rotation limitをもっと簡潔にした

--- src/tickChain.tsx
+++ src/tickChain.tsx
@@ -1,4 +1,4 @@
-import { WorldPosition, WorldRotation } from "./Transform";
+import { WorldPosition } from "./Transform";
 import { movePoint } from "./movePoint";
 import { chainLengths } from "./App";
 
@@ -48,49 +48,42 @@ function backward(
     const curr = chain[i - 1];
     const next = chain[i];
     const len = chainLens[i - 1];
-    const offset = {
-      x: curr.x - prev?.x,
-      y: curr.y - prev?.y,
+    const prevVec = {
+      x: curr.x - (prev?.x ?? 0),
+      y: curr.y - (prev?.y ?? 0),
     };
-    const limit = rotationLimits && prev ? rotationLimits[i - 1] : undefined;
-    let newPoint = movePoint(curr, next, len) as WorldPosition;
+    const nextVec = {
+      x: next.x - curr.x,
+      y: next.y - curr.y,
+    };
 
-    //limitはここで使ったほうが良い
-    if (limit) {
-      const nextOffset = {
-        x: newPoint.x - curr.x,
-        y: newPoint.y - curr.y,
-      };
-      const newAngle = getDiffAngle(offset, nextOffset, limit);
-      const newOffset = rotate(nextOffset, newAngle) as WorldPosition;
-      newPoint = {
-        x: curr.x + newOffset.x,
-        y: curr.y + newOffset.y,
-      } as WorldPosition;
-    }
-    chain[i] = newPoint;
+    //関節の角度は、前後の点によって定める。-PI ~ PI
+    const prevAngle = Math.atan2(prevVec.y, prevVec.x);
+    const angle = getAngleBetween2Vector(prevVec, nextVec);
+
+    const limit = rotationLimits?.[i - 1];
+    const limitedAngle = limit
+      ? Math.max(limit[0], Math.min(limit[1], angle))
+      : angle;
+    const newOffset = rotate({ x: len, y: 0 }, prevAngle + limitedAngle);
+
+    chain[i] = {
+      x: curr.x + newOffset.x,
+      y: curr.y + newOffset.y,
+    } as WorldPosition;
   }
   return chain;
 }
 
-function getDiffAngle(
-  prevVec: Position,
-  nextVec: Position,
-  [min, max]: [min: number, max: number]
-): number {
-  const outerProduct = prevVec.x * nextVec.y - prevVec.y * nextVec.x;
-  const prevLen = Math.hypot(prevVec.x, prevVec.y);
-  const nextLen = Math.hypot(nextVec.x, nextVec.y);
-  const sin = outerProduct / (prevLen * nextLen);
-  const angle = Math.asin(sin);
-
-  if (angle < min) {
-    return min - angle;
-  } else if (angle > max) {
-    return max - angle;
-  } else {
-    return 0;
+function getAngleBetween2Vector(prevVec: Position, nextVec: Position): number {
+  const theta =
+    Math.atan2(nextVec.y, nextVec.x) - Math.atan2(prevVec.y, prevVec.x);
+  if (theta < -Math.PI) {
+    return theta + Math.PI * 2;
+  } else if (theta > Math.PI) {
+    return theta - Math.PI * 2;
   }
+  return theta;
 }
 
 function rotate(v: Position, rotation: number) {
